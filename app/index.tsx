156d7049ac56b9fb638d7c0345543e46112df63c
AddChanin <102175316+AddChanin@users.noreply.github.com>
2025-11-01 11:07:57 +0700

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { StyleSheet, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SetCounter from "./component/setCounter";
